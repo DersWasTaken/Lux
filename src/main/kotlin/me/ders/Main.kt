62992bcaee9d7aecc347cbe661e427046429544f
DersWasTaken <isthatder@gmail.com>
2023-12-23 13:57:03 -0700

    * Minor Changes *
    [-] Remove Main.kt server initialization

--- src/main/kotlin/me/ders/Main.kt
+++ src/main/kotlin/me/ders/Main.kt
@@ -1,36 +1,5 @@
 package me.ders
 
-import me.ders.event.on
-import net.minestom.server.MinecraftServer
-import net.minestom.server.coordinate.Pos
-import net.minestom.server.entity.GameMode
-import net.minestom.server.event.player.PlayerLoginEvent
-import net.minestom.server.instance.LightingChunk
-import net.minestom.server.instance.block.Block
-import net.minestom.server.instance.generator.GenerationUnit
-
 fun main() {
-    val server = MinecraftServer.init()
 
-    server.start("0.0.0.0", 25565)
-
-    val instanceManager = MinecraftServer.getInstanceManager()
-
-    val instanceContainer = instanceManager.createInstanceContainer()
-
-    instanceContainer.setGenerator { unit: GenerationUnit ->
-        unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK)
-    }
-
-    instanceContainer.setChunkSupplier { instance ,chunkX, chunkZ ->
-        LightingChunk(instance,chunkX,chunkZ)
-    }
-
-    on<PlayerLoginEvent> {
-        execute {
-            setSpawningInstance(instanceContainer)
-            player.respawnPoint = Pos(0.0, 42.0, 0.0)
-            player.gameMode = GameMode.CREATIVE
-        }
-    }
 }
